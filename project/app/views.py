e3b002515bb335ddbce18bdf0d096a6a10b9e04a
smritijaiswal2007 <smritijais999@gmail.com>
2026-01-14 21:49:32 +0530

    _

--- project/app/views.py
+++ project/app/views.py
@@ -3,10 +3,16 @@ from django.http import JsonResponse
 
 # Create your views here.
 def json_response(req):
-    data = {
-        'active1' : True,
-        'active2' : False,
-        'active3' : None,
-        
-    }
-    return JsonResponse(data)
+    # data = {
+    #     'active1' : True,
+    #     'active2' : False,
+    #     'active3' : None,   
+    # }
+    data = 10
+    # data = True (only set and frozen set datatype cannot be send in json )
+    # data = None
+    # data = [ 10,20,20,40,] list 
+    # data = [ 10,20,20,40,'smriti'] list with string value
+    # data = (10,20,20,40,'smriti') tupple  
+    # //important = json data bhejte time chahe list me bhejo ya tupple mai output hmesha list format mai aayega
+    return JsonResponse(data,safe=False)

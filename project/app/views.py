ee54a509ef297367e40eae48caacdef3f48d9d81
smritijaiswal2007 <smritijais999@gmail.com>
2026-04-03 23:25:04 +0530

    _

--- project/app/views.py
+++ project/app/views.py
@@ -7,7 +7,7 @@ def json_response(req):
     #     'active1' : True,
     #     'active2' : False,
     data = 654
-    data1 = 987
+    data1 = 452
     # data = True (only set and frozen set datatype cannot be send in json )
     # data = None
     # data = [ 10,20,20,40,] list 
